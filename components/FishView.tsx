202e0f1d7d888ca21abc1b4091605f9514915f4b
Jaegersvold <47804963+Jaegersvold@users.noreply.github.com>
2022-04-27 18:06:27 -0500

    Removed error message

--- components/FishView.tsx
+++ components/FishView.tsx
@@ -4,42 +4,44 @@ import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 import axios from "axios";
-import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
 
-import {Fish, GetFishResponse} from "../common/types";
+import { Fish, GetFishResponse } from "../common/types";
 
 const columns: GridColDef[] = [
-  {field: 'id', headerName: 'ID', 'width': 100},
-  { field: 'name', headerName: 'Fish Name', width: 200, editable: true },
+  { field: "id", headerName: "ID", width: 100 },
+  { field: "name", headerName: "Fish Name", width: 200, editable: true },
   {
-    field: 'avg_length',
-    headerName: 'Average Length (Inches)',
-    type: 'number',
+    field: "avg_length",
+    headerName: "Average Length (Inches)",
+    type: "number",
     width: 200,
     editable: true,
   },
 ];
 
 const FishView = () => {
-  const [fish, setFish] = useState<Fish[]>([])
+  const [fish, setFish] = useState<Fish[]>([]);
 
   useEffect(() => {
-      axios.get("/api/get_fish").then(res => {
-        setFish(res.data.rows)
-      }).catch(e => console.log(e))
+    axios
+      .get("/api/get_fish")
+      .then((res) => {
+        setFish(res.data.rows);
+      })
+      .catch((e) => console.log(e));
   }, []);
 
   const handleRowUpdate = (new_values: any, old_values: any) => {
-    console.log(new_values)
-    console.log(old_values)
+    console.log(new_values);
+    console.log(old_values);
 
-    axios.put("/api/update_fish", new_values).catch(err => console.log(err))
-    return new_values
-  }
-
+    axios.put("/api/update_fish", new_values).catch((err) => console.log(err));
+    return new_values;
+  };
 
   const Grid = () => (
-    <div style={{ height: 400, width: '100%' }}>
+    <div style={{ height: 400, width: "100%" }}>
       <DataGrid
         experimentalFeatures={{ newEditingApi: true }}
         getRowId={(row) => row.id}
@@ -53,16 +55,9 @@ const FishView = () => {
     </div>
   );
 
-  const Error = () => (
-    <Box>
-      <p>{error}</p>
-    </Box>
-  );
-
   return (
     <Box>
       <Grid />
-      <Error />
     </Box>
   );
 };
